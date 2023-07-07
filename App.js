9fecb7b047ae86c9acea52e03434e4891b24aadf
unknown <prasadabnave8989@gmail.com>
2023-07-07 17:23:15 +0530

    episode1

---
+++ App.js
@@ -0,0 +1,23 @@
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React");
+
+//        const root = ReactDOM.createRoot(document.getElementById("root"));
+       
+//        root.render(heading);
+
+
+
+const parent = React.createElement(
+    "div",{id:"parent"}, 
+    React.createElement(
+        "div",
+        {id:"child"},[
+    React.createElement("h1",{},"I am in h1  tag"),
+    React.createElement("h2",{},"I am in h2  tag")]
+    )
+ );
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
